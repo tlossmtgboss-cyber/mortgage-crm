ceb75735138bd4903d1e3f4eed42f23e4ef2ae55
tlossmtgboss-cyber <tlossmtgboss@gmail.com>
2025-10-25 15:41:09 -0400

    Add EventLog SQLAlchemy model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
 
+Base = declarative_base()
+
+class EventLog(Base):
+    __tablename__ = 'event_logs'
+    id = Column(Integer, primary_key=True, index=True)
+    event_type = Column(String, index=True)  # "SMS", "CALL", "EMAIL"
+    from_number = Column(String)
+    body_or_status = Column(String)
+    ts = Column(DateTime, default=datetime.datetime.utcnow)
