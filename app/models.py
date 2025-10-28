eb03c57606cb6edb9480edeeb65d27c5a356d175
tlossmtgboss-cyber <tlossmtgboss@gmail.com>
2025-10-28 04:39:36 -0400

    Fix SQLAlchemy deprecation: Update declarative_base import
    Updated import from sqlalchemy.ext.declarative to sqlalchemy.orm to fix deprecation warning and use modern SQLAlchemy standards

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, Text, Float, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 import datetime
 
 Base = declarative_base()
